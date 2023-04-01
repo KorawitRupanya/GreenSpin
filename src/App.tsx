6ace8d314561861ea973539b9327ad2578bc0fa7
Korawit Rupanya <32285708+KorawitRupanya@users.noreply.github.com>
2023-04-01 22:00:47 +0300

    Make changes to Spinnova project

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import * as React from 'react';
 import { useRef } from 'react'
 import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
 
+const woodWest: string = require("./assets/WoodBiowaste.svg").default;
+const simpleLeaf: string = require("./assets/Simpleleaf.svg").default;
+const pulp: string = require("./assets/Pulp.svg").default;
+const white_fiber:string = require( "./assets/White_Fiber.svg").default;
+const fabric: string = require("./assets/FabricMaterial.svg").default;
+const fashion: string = require("./assets/Fashion.svg").default;
+const spinnovaLogo: string = require("./assets/Spinnova_logo_white.svg").default;
+
 // Little helpers ...
 const url = (name: string, wrap = false) =>
   `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
@@ -9,10 +17,13 @@ const url = (name: string, wrap = false) =>
 export default function App() {
   const parallax = useRef<IParallax>(null!)
   return (
-    <div style={{ width: '100%', height: '100%', background: '#253237' }}>
-      <Parallax ref={parallax} pages={3}>
-        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
-        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
+    <div style={{ width: '100%', height: '100%', background: '#C0EC83' }}>
+      <Parallax ref={parallax} pages={6}>
+        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#A7E074' }} />
+        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#96CC39' }} />
+        <ParallaxLayer offset={3} speed={1} style={{ backgroundColor: '#6BA32D' }} />
+        <ParallaxLayer offset={4} speed={1} style={{ backgroundColor: '#547A1D' }} />
+        <ParallaxLayer offset={5} speed={1} style={{ backgroundColor: '#3B5C0A' }} />
 
         <ParallaxLayer
           offset={0}
@@ -24,38 +35,34 @@ export default function App() {
           }}
         />
 
-        <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
-          <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
+        <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.6 }}>
+          <img src={simpleLeaf} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
+          <img src={simpleLeaf} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
+          <img src={simpleLeaf} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
+          <img src={simpleLeaf} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
+          <img src={simpleLeaf} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
+          <img src={simpleLeaf} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
+          <img src={simpleLeaf} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
+          <img src={simpleLeaf} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
+          <img src={simpleLeaf} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
+          <img src={simpleLeaf} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
+          <img src={simpleLeaf} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer
-          offset={2.5}
+          offset={5.5}
           speed={-0.4}
           style={{
             display: 'flex',
@@ -67,16 +74,6 @@ export default function App() {
         </ParallaxLayer>
 
         <ParallaxLayer
-          offset={2}
-          speed={-0.3}
-          style={{
-            backgroundSize: '80%',
-            backgroundPosition: 'center',
-            backgroundImage: url('clients', true),
-          }}
-        />
-
-        <ParallaxLayer
           offset={0}
           speed={0.1}
           onClick={() => parallax.current.scrollTo(1)}
@@ -85,7 +82,17 @@ export default function App() {
             alignItems: 'center',
             justifyContent: 'center',
           }}>
-          <img src={url('server')} style={{ width: '20%' }} />
+        <div style={{width: '100%'}}>
+          <h1 style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'white',
+          }}>
+          Wood Waste
+        </h1>
+          <img src={woodWest} style={{ width: '100%' }} />
+        </div>
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -97,11 +104,66 @@ export default function App() {
             alignItems: 'center',
             justifyContent: 'center',
           }}>
-          <img src={url('bash')} style={{ width: '40%' }} />
+        <div style={{width: '100%'}}>
+          <h1 style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'white',
+          }}>
+          Paper Pulp
+        </h1>
+          <img src={pulp} style={{ width: '100%' }} />
+        </div>
         </ParallaxLayer>
 
         <ParallaxLayer
           offset={2}
+          speed={0.1}
+          onClick={() => parallax.current.scrollTo(1)}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+        <div style={{width: '100%'}}>
+          <h1 style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'white',
+          }}>
+          Spinnova Fiber
+        </h1>
+          <img src={white_fiber} style={{ width: '100%' }} />
+        </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={0.1}
+          onClick={() => parallax.current.scrollTo(2)}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+            <div>
+            <h1 style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'white',
+          }}>
+          Fabric Material
+        </h1>
+          <img src={fabric} style={{ width: '100%' }} />
+            </div>
+           
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={4}
           speed={-0}
           style={{
             display: 'flex',
@@ -109,7 +171,29 @@ export default function App() {
             justifyContent: 'center',
           }}
           onClick={() => parallax.current.scrollTo(0)}>
-          <img src={url('clients-main')} style={{ width: '40%' }} />
+         <div>
+            <h1 style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'white',
+          }}>
+          Fashion Apparel
+        </h1>
+          <img src={fashion} style={{ width: '100%' }} />
+            </div>
+        </ParallaxLayer>
+        
+        <ParallaxLayer
+          offset={5}
+          speed={-0}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          onClick={() => parallax.current.scrollTo(0)}>
+          <img src={spinnovaLogo} style={{ width: '40%' }} />
         </ParallaxLayer>
       </Parallax>
     </div>
